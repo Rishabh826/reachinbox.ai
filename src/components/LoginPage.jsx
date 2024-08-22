5ac0cf8d4e107a291e797c83deb94f4b378c937d
Rishabh826 <rishabhjangid0707@gmail.com>
2024-08-22 17:19:05 +0530

    change login

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
 
   const handleGoogleLogin = () => {
     // Redirect the user to the Google OAuth login page
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/onebox";
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reachinbox-ai-five.vercel.app/";
   };
 
   return (
